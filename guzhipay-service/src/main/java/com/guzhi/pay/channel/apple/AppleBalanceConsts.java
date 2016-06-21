15da197fb828f72249cc63dd84b2be1f2be73c23
李子才 <davis-sz@users.noreply.github.com>
2016-06-21 14:30:01 +0800

    project init

---
+++ guzhipay-service/src/main/java/com/guzhi/pay/channel/apple/AppleBalanceConsts.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012 guzhi.com. 
+ * All Rights Reserved.
+ * This program is the confidential and proprietary information of 
+ * guzhi. ("Confidential Information").  You shall not disclose such
+ * Confidential Information and shall use it only in accordance with
+ * the terms of the license agreement you entered into with guzhi.com.
+ */
+package com.guzhi.pay.channel.apple;
+
+/**
+ * @author administrator
+ * 
+ */
+public interface AppleBalanceConsts {
+
+    // 正式验证地址
+    static final String PRODUCVERIFYRECEIPTURl = "https://buy.itunes.apple.com/verifyReceipt?";
+
+    // 测试验证地址
+    static final String TESTVERIFYRECEIPTURl = "https://sandbox.itunes.apple.com/verifyReceipt?";
+
+    static final String RECEIPTDATA = "receipt-data";
+
+    static final String STATUS = "status";
+
+    static final String SUCCESS = "0";
+
+    static final String QUANTITY = "quantity";
+
+    static final String PRODUCT_ID = "product_id";
+
+    static final String RECEIPT = "receipt";
+
+    static final String UNIT_PRICE = "unitPrice";
+}
