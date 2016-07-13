efe76c9dd80cdaf51c793f43cf3eca39d0f8682c
李子才 <davis-sz@users.noreply.github.com>
2016-07-13 18:53:48 +0800

    初始导入。

---
+++ src/main/java/com/blemobi/payment/global/Constant.java
@@ -0,0 +1,287 @@
+package com.blemobi.payment.global;
+
+/**
+ * @author 李子才<davis.lee@blemobi.com>
+ * 这是跟前端App相关的全局变量值的工具类，前端App需要的参数，都是重这里获取。
+ * 系统启动时，从 consul服务器获取到的运行参数，统一保存在这里.
+ * 系统中其他的子模块需要用参数信息，统一从这里获取.
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.blemobi.payment.consul.ConsulChangeListener;
+import com.google.common.base.Strings;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class Constant  {
+	
+	//通用键值的key的声明
+	private static final String KEY_REDIS_USER_ADDR = "redis_user_addr";
+	private static final String KEY_REDIS_USER_AUTH = "redis_user_auth";
+	private static final String KEY_HEALTH_CHECK_PORT = "health_check_port";
+	private static final String KEY_JETTY_PORT = "jetty_port";
+	
+	// 我们对外服务的业务所在的包目录。目录之外，是禁止访问
+	private static final String OutServicePermitPackagePath = "com.blemobi.netdisk.rest"; 
+	// Consul定时任务间隔时间，单位：毫秒
+	private static final long ConsulIntervalTime = 1000 * 30;
+
+	
+	private static String[][] accountInfo = null;
+	private static List<String> errorAccount = new ArrayList();
+	
+	private static String[][] ossInfo = null;
+	private static List<String> errorOss = new ArrayList();
+	
+	private static String[][] loginInfo = null;
+	private static List<String> errorLogin = new ArrayList();
+	
+	// 定义我们自己的Jetty服务的端口
+	private static int jettyServerPort = 9015; 
+
+	// 定义Redis服务的IP地址
+	private static String redisServerIP = null; 
+	// 定义Redis服务的端口
+	private static String redisServerPort = null; 
+	// 定义Redis服务的认证信息
+	private static String redisServerAuth = null; 
+	// 聊天服务器的健康发现的端口
+	private static int contactServiceHealthPort = 19015;
+	
+	//创建Consul服务器的适配器对象，该对象能接受从consul服务器传递过来的配置信息变更通知。
+	private static ConsulChangeListener adapter = new ConsulChangeListener(){
+		
+		public void onEnvChange(Map<String, String> prop) {
+			
+			try{
+				redisServerAuth = prop.get(KEY_REDIS_USER_AUTH);
+				
+				contactServiceHealthPort  = Integer.parseInt(prop.get(KEY_HEALTH_CHECK_PORT));
+				
+				jettyServerPort  = Integer.parseInt(prop.get(KEY_JETTY_PORT));
+				
+				String redisUserAddr = prop.get(KEY_REDIS_USER_ADDR);; // 定义Redis服务的IP地址和端口
+				
+				int point = redisUserAddr.indexOf(':');
+				if(point < 1 || point >= (redisUserAddr.length()-1)){
+					log.info("redis_user_addr format error!");
+				}else{
+					redisServerIP = redisUserAddr.substring(0, point);
+					redisServerPort = redisUserAddr.substring(point+1);
+				}
+			}catch(RuntimeException e){
+				log.info("Get Constant Info error! Info=(Consul Server error or Local Info not find.)");
+				log.info("Please check config information.)");
+				throw e;
+			}
+			
+			
+		}
+
+		public void onServiceChange(String serviceName, String[][] serverInfo) {
+			if(serviceName.equals("account")){
+				accountInfo = serverInfo;
+				errorAccount.clear();
+			}
+			
+			if(serviceName.equals("oss")){
+				ossInfo = serverInfo;
+				errorOss.clear();
+			}
+			
+			if(serviceName.equals("login")){
+				loginInfo = serverInfo;
+				errorLogin.clear();
+			}
+			
+		}
+	};
+	
+	/**
+	 * 获取账户服务的服务器列表。
+	 * 整个过程是：先从consul中获取所有的账户服务器，然后再筛选健康的服务器，再从健康中随机取一台做为服务返回给用户。
+	 * @return 返回“健康正常账户服务”的服务器信息。String[0]为IP地址，String[1]为端口。
+	 */
+	public static String[] getAccountServer() {
+		String[][] healthAccount = getOnlineServer(accountInfo,errorAccount);
+		int ramdom = (int)(Math.random() * healthAccount.length);
+		return healthAccount[ramdom];
+//		return new String[]{"192.168.1.241","9002"};// 账户系统IP和port信息
+	}
+	
+	/**
+	 * 获取登录服务的服务器列表。
+	 * 整个过程是：先从consul中获取所有的账户服务器，然后再筛选健康的服务器，再从健康中随机取一台做为服务返回给用户。
+	 * @return 返回“健康正常账户服务”的服务器信息。String[0]为IP地址，String[1]为端口。
+	 */
+	public static String[] getLoginServer() {
+		
+		String[][] healthAccount = getOnlineServer(loginInfo,errorLogin);
+		int ramdom = (int)(Math.random() * healthAccount.length);
+		
+		return healthAccount[ramdom];
+//		return new String[]{"192.168.1.241","9001"};// 账户系统IP和port信息
+	}
+	
+	/**
+	 * 获取Oss服务的服务器列表。
+	 * 整个过程是：先从consul中获取所有的好友服务器，然后再筛选健康的服务器，再从健康中随机取一台做为服务返回给用户。
+	 * @return 返回“健康正常Oss服务”的服务器信息。String[0]为IP地址，String[1]为端口。
+	 */
+	public static String[] getOssServer() {
+		String[][] healthOss = getOnlineServer(ossInfo,errorOss);
+		int ramdom = (int)(Math.random() * healthOss.length);
+		return healthOss[ramdom];
+//		return new String[]{"192.168.1.241","9008"};// 好友系统IP和port信息
+	}
+	
+	/**
+	 * 从consul服务器获取Redis服务器的信息。
+	 * @param host 是consul服务器的域名或IP地址。
+	 * @return 返回Redis服务器的信息。String[0]为IP地址，String[1]为端口。
+	 */
+	public static String[] getRedisServer() {
+		return new String[]{redisServerIP,redisServerPort};
+	}
+	
+	/**
+	 * 从consul服务器获取Redis服务器的信息。
+	 * @return 返回Redis服务器的的Token认证信息。
+	 */
+	public static String getRedisUserAuth() {
+		return redisServerAuth;// Redis服务器的认证
+	}
+	
+	/**
+	 * 报告有异常的账户服务器，该服务器将记录在异常服务器列表中，当用户获取账户服务器时，则过滤掉该服务器。
+	 * 记录服务器的信息为(ip+port)的方式来代表一台服务器。
+	 * @param addr 有异常的账户服务器的IP地址。
+	 * @param port 有异常的账户服务器的Port端口。
+	 */
+	public static void reportErrorAccountServer(String addr,int port) {
+		//如果是系统不是local模式，才对异常服务器列表过滤。
+		//因此本地模式，该服务器只有一台，如果提示有服务器异常，该服务器就没有服务器了！
+		if(!Strings.isNullOrEmpty(System.getProperty("EnvMode", ""))){
+			errorAccount.add(addr+"-"+port);
+		}
+	}
+	
+	/**
+	 * 报告有异常的登录服务器，该服务器将记录在异常服务器列表中，当用户获取账户服务器时，则过滤掉该服务器。
+	 * 记录服务器的信息为(ip+port)的方式来代表一台服务器。
+	 * @param addr 有异常的账户服务器的IP地址。
+	 * @param port 有异常的账户服务器的Port端口。
+	 */
+	public static void reportErrorLoginServer(String addr,int port) {
+		//如果是系统不是local模式，才对异常服务器列表过滤。
+		//因此本地模式，该服务器只有一台，如果提示有服务器异常，该服务器就没有服务器了！
+		if(!Strings.isNullOrEmpty(System.getProperty("EnvMode", ""))){
+			errorLogin.add(addr+"-"+port);
+		}
+	}
+	
+	/**
+	 * 报告有异常的好友服务器，该服务器将记录在异常服务器列表中，当用户获取好友服务器时，则过滤掉该服务器。
+	 * 记录服务器的信息为(ip+port)的方式来代表一台服务器。
+	 * @param addr 有异常的好友服务器的IP地址。
+	 * @param port 有异常的好友服务器的Port端口。
+	 */
+	public static void reportErrorFriendServer(String addr,int port) {
+		//如果是系统不是local模式，才对异常服务器列表过滤。
+		//因此本地模式，该服务器只有一台，如果提示有服务器异常，该服务器就没有服务器了！
+		if(!Strings.isNullOrEmpty(System.getProperty("EnvMode", ""))){
+			errorOss.add(addr+"-"+port);
+		}
+	}
+
+	/**
+	 * 从某个服务的consul服务器列表中，过来出来健康的服务器列表。
+	 * @param all 某个服务的所有服务器，也就是从consul服务器中获取到该服务的所有服务器。
+	 * @param error 某个服务的所有服务器中，有异常的服务器类别。
+	 * @return 返回某个服务的健康的服务器的列表，也就是“all集合”中排除掉“error集合”后 ，剩余的结果。String[i][0]为IP地址，String[i][1]为端口。
+	 */
+	private static String[][] getOnlineServer(String[][] all, List<String> error) {
+		int count = all.length;
+		String[][] buff = new String[count][2];
+		int index = 0;
+		
+		String[] errorList = new String[error.size()];
+		error.toArray(errorList);
+		
+		for(int i=0;i<count;i++){
+			String v = buff[i][0] + "-" + buff[i][1];
+			if(!isExist(errorList,v)){
+				buff[index][0] = all[i][0];
+				buff[index][1] = all[i][1];
+				index++;
+			}
+		}
+
+		String[][] rtn = new String[index][2];
+		System.arraycopy(buff, 0, rtn, 0, rtn.length);
+		
+		return rtn;
+	}
+
+	/**
+	 * 判断集合里是否存在某个元素。
+	 * @param errorList 大集合。
+	 * @param v 待查找存在的字符串对象。
+	 * @return 如果存在，则返回true。
+	 */
+	private static boolean isExist(String[] errorList, String v) {
+		for(int i=0;i<errorList.length;i++){
+			if(errorList[i].equals(v)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	/**
+	 * 获取jesry容器中允许访问的包名目录。默认值是com.blemobi.contact.rest。
+	 * @return 授权访问的包名。
+	 */
+	public static String getOutservicepermitpackagepath() {
+		return OutServicePermitPackagePath;
+	}
+
+
+
+	/**
+	 * 获取访问聊天系统的服务端口，默认值是9006。
+	 * @return 服务端口。
+	 */
+	public static int getJettyServerPort() {
+		return jettyServerPort;
+	}
+
+	/**
+	 * 获取访问consul服务器的间隔时间。
+	 * @return 间隔时间，单位是毫秒的值。
+	 */
+	public static long getConsulIntervaltime() {
+		return ConsulIntervalTime;
+	}
+
+
+	/**
+	 * 获取注册的适配器，当适配器注册登记到ConsulManager管理类后，则可以收到Consul的配置信息变更的通知。
+	 * @return 适配器对象。
+	 */
+	public static ConsulChangeListener getAdapter() {
+		return adapter;
+	}
+	
+	/**
+	 * 获取聊天服务器的健康发现的端口。
+	 * @return 网络Socket端口值。
+	 */
+	public static int getChatServiceHealthPort() {
+		return contactServiceHealthPort ;
+	}
+}
---
+++ src/main/java/com/blemobi/payment/global/PathGlobal.java
@@ -0,0 +1,39 @@
+package com.blemobi.payment.global;
+
+/**
+ * 统一定义访问外部api地址
+ * 
+ * @author 赵勇<andy.zhao@blemobi.com>
+ */
+public class PathGlobal {
+
+	// 根据uuid和token获取用户信息，成功返回Puser
+	public static final String GetUser = "/account/user/profile";
+
+	// 验证对方是不是好友，成功返回Puser
+	public static final String GetFriend = "/account/get/friend";
+
+	// 获取群信息（包含群成员openId），成功返回PGroup
+	public static final String GetGroup = "/account/get/group";
+
+	// 获取其他用户信息，成功返回PGroup
+	public static final String GetOtherUser = "/account/get/user";
+	
+	// 获取其他用户信息，成功返回PGroup
+	public static final String GetContactUUID = "/v1/login/inside/uuid";
+		
+	// 获取用户的好友列表信息，成功返回PuserList
+	public static final String GetFriendUserList = "/social/listfriends";
+	
+	// 获取用户的Vip等级的接口，成功返回PuserList
+	public static final String GetUserLevel = "/account/user/level";
+	
+	// 获取文件上传的接口，成功返回PuserList
+	public static final String GetOssUploadFileUrl = "/oss/newfileurl";
+	
+	// 获取文件上传的接口，成功返回PuserList
+	public static final String GetOssUploadFileUrls = "/oss/newfileurls";
+		
+	// 获取文件下载的url的接口，成功返回PuserList
+	public static final String GetOssDownloadFileUrl = "/oss/downloadurl";
+}
