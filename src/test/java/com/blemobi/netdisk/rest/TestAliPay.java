c8678c15e52ee038b31198ad723661e1ee6aecd0
李子才 <davis-sz@users.noreply.github.com>
2016-07-26 17:52:00 +0800

    修改测试用例

--- src/test/java/com/blemobi/netdisk/rest/TestAliPay.java
+++ src/test/java/com/blemobi/netdisk/rest/TestAliPay.java
@@ -1,5 +1,7 @@
 package com.blemobi.netdisk.rest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.blemobi.payment.core.PaymentManager;
@@ -7,22 +9,25 @@ import com.blemobi.payment.rest.alipay.AliPayUtil;
 import com.blemobi.sep.probuf.PaymentProtos.PAlipayOrderInfo;
 import com.blemobi.sep.probuf.ResultProtos.PMessage;
 import com.blemobi.sep.probuf.ResultProtos.PResult;
+import com.ecwid.consul.v1.ConsulClient;
 
 import lombok.extern.log4j.Log4j;
 @Log4j
 
 public class TestAliPay {
-
-	@Test
-	public void testAliPay() throws Exception {
-		// TODO Auto-generated method stub
+	@Before
+	public void setUp() throws Exception {
 		String[] arg = new String[] { "-env", "local" };
 		PaymentManager.main(arg);
 		try {
 			Thread.sleep(3 * 1000);
 		} catch (InterruptedException e) {
 		}
-
+	}
+	
+	@Test
+	public void testAliPay() throws Exception {
+		// TODO Auto-generated method stub
 		String uuid = "0efe519d-cddf-412c-a5e0-2e8f14f80edb";
 		String token = "EiBmN2UzMzM5ZWFiOGZmZTJkZTg5MTE2NGQ2YjJiOGRiMBjYtte8BQ==";
 		String orderSubject = "Subject1";
@@ -44,23 +49,10 @@ public class TestAliPay {
 			log.info("getOrderNo=["+orderInfo.getOrderNo()+"]");
 			log.info("getPayInfo=["+orderInfo.getPayInfo()+"]");
 		}
-		
-		
-		
-		System.exit(0);
-		
-		
-		
-		
-//		Param orderBody=[充值1]
-//				Param orderSubject=[充值1]
-//				Param orderPrice=[1]
-//				Cookie uuid=[0efe519d-cddf-412c-a5e0-2e8f14f80edb]
-//				Cookie token=[EiBmN2UzMzM5ZWFiOGZmZTJkZTg5MTE2NGQ2YjJiOGRiMBjYtte8BQ==]
-		
-		
-		
-		
 	}
-
+	
+	@After
+	public void tearDown() {
+		System.exit(0);
+	}
 }
