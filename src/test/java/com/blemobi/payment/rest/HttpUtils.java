9f21ddad7697c9b5b5ccb10b74d83cf084306fa1
HUNTER.POON <hunter.pan@foxmail.com>
2017-03-07 19:43:05 +0800

    UPD：更新测试

---
+++ src/test/java/com/blemobi/payment/rest/HttpUtils.java
@@ -0,0 +1,147 @@
+/******************************************************************
+ *
+ *    
+ *    Package:     com.blemobi.payment.rest
+ *
+ *    Filename:    HttpUtils.java
+ *
+ *    Description: TODO
+ *
+ *    @author:     HUNTER.POON
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2017年3月7日 下午5:12:50
+ *
+ *    Revision:
+ *
+ *    2017年3月7日 下午5:12:50
+ *
+ *****************************************************************/
+package com.blemobi.payment.rest;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.Cookie;
+
+/**
+ * @ClassName HttpUtils
+ * @Description TODO
+ * @author HUNTER.POON
+ * @Date 2017年3月7日 下午5:12:50
+ * @version 1.0.0
+ */
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+ 
+/**
+ * Created by yinfx on 15-9-16.
+ */
+public class HttpUtils {
+    public static final MediaType PROTO_BUF = MediaType.parse("application/x-protobuf; charset=utf-8");
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final MediaType XML = MediaType.parse("application/xml; charset=utf-8");
+    private static HttpUtils instance;
+    private OkHttpClient client;
+ 
+    private HttpUtils() {
+        client = new OkHttpClient();
+        client.setConnectTimeout(20, TimeUnit.SECONDS);//设置请求超时，20s
+        client.setReadTimeout(20, TimeUnit.SECONDS);//设置读取超时，20s
+        client.setWriteTimeout(20, TimeUnit.SECONDS);//设置写入超时，20s
+    }
+ 
+    public synchronized static HttpUtils getInstance() {
+        return instance == null ? instance = new HttpUtils() : instance;
+    }
+ 
+    /**
+     * 提交POST请求
+     *
+     * @param url       地址
+     * @param mediaType 请求体文本类型
+     * @param content   请求体
+     * @param headers   请求头
+     * @return
+     * @throws IOException
+     */
+    public String post(String url, MediaType mediaType, String content, String... headers) throws IOException {
+        RequestBody body = RequestBody.create(mediaType, content);
+        Request.Builder builder = new Request.Builder().url(url);
+        if (headers != null && headers.length > 0 && headers.length % 2 == 0) {
+            for (int i = 0; i < headers.length - 1; i++)
+                if (i % 2 == 0) builder.addHeader(headers[i], headers[i + 1]);
+        }
+        Request request = builder.post(body).build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+ 
+    
+    public String post(String url, MediaType mediaType, byte[] data, Map<String, String> cookies) throws IOException {
+        Request.Builder builder = new Request.Builder().url(url);
+        if(cookies != null && cookies.size() > 0){
+            List<Cookie> cookieList = new ArrayList<Cookie>();
+            for(Entry<String, String> entry : cookies.entrySet()){
+                Cookie cookie = new Cookie(entry.getKey(), entry.getValue());
+                cookieList.add(cookie);
+            }
+            builder = builder.header("Cookie", cookieHeader(cookieList));
+        }
+        Request request = builder.post(RequestBody.create(mediaType, data)).build();
+        Response response = client.newCall(request).execute();
+        if(response.isSuccessful()){
+            System.out.println(response.body().toString());
+        }else{
+            System.err.println("error ");
+        }
+        
+        return response.body().string();
+    }
+    
+    private static String cookieHeader(List<Cookie> cookies) {
+        StringBuilder cookieHeader = new StringBuilder();
+        for (int i = 0, size = cookies.size(); i < size; i++) {
+            if (i > 0) {
+                cookieHeader.append("; ");
+            }
+            Cookie cookie = cookies.get(i);
+            cookieHeader.append(cookie.getName()).append('=').append(cookie.getValue());
+        }
+        return cookieHeader.toString();
+    }
+    
+    /**
+     * 提交POST请求
+     *
+     * @param url     地址
+     * @param content 请求体
+     * @return
+     * @throws IOException
+     */
+    public String post(String url, String content) throws IOException {
+        return post(url, HttpUtils.JSON, content, null);
+    }
+ 
+    /**
+     * 提交GET请求
+     *
+     * @param url 地址
+     * @return
+     * @throws IOException
+     */
+    public String get(String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
--- src/test/java/com/blemobi/payment/rest/LotteryProcessTest.java
+++ src/test/java/com/blemobi/payment/rest/LotteryProcessTest.java
@@ -1,27 +1,28 @@
 package com.blemobi.payment.rest;
 
-import static org.junit.Assert.*;
-
-import javax.servlet.http.Cookie;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.blemobi.library.client.BaseHttpClient;
-import com.blemobi.library.client.LocalHttpClient;
 import com.blemobi.payment.core.PaymentManager;
-import com.blemobi.sep.probuf.PaymentProtos.PAcceptPrize;
-import com.blemobi.sep.probuf.ResultProtos.PMessage;
+import com.blemobi.sep.probuf.PaymentProtos.PShuffle;
 
+import lombok.extern.log4j.Log4j;
+@Log4j
 public class LotteryProcessTest {
-	private Cookie[] cookies;
-
+    private static final String BASE_URL = "http://192.168.7.245";
+    //private static final String BASE_URL = "http://127.0.0.1";
+    private static int port = 9014;
+    private static Map<String, String> cookies = new HashMap<String, String>();
 	@Before
 	public void setup() {
-		cookies = new Cookie[2];
-		cookies[0] = new Cookie("uuid", "1468419313301436967");
-		cookies[1] = new Cookie("token", "98e7eee14df39598c458fbbfa04843cb");
-
+		cookies.put("uuid", "1468419313301436967");
+		cookies.put("token", "98e7eee14df39598c458fbbfa04843cb");
 		String[] arg = new String[] { "-env", "local" };
 		try {
 		    PaymentManager.main(arg);
@@ -30,10 +31,40 @@ public class LotteryProcessTest {
 			e.printStackTrace();
 		}
 	}
+	private String getPath(String url){
+	    return BASE_URL + ":" + port + url;
+	}
 
 	@Test
-	public void test() throws Exception {
-	    /*StringBuffer path = new StringBuffer("/v1/payment/lottery/create");
+	public void test(){
+	    
+	    String uri = "/v1/payment/lottery/shuffle";
+        PShuffle.Builder builder = PShuffle.newBuilder();
+        List<String> regions = new ArrayList<String>();
+        regions.add("CN;0001");
+        regions.add("CN;0002");
+        builder.setBonus(50).setGender(-1).addAllRegion(regions).setRemark("测试").setTitle("三月红包").setTotAmt(500).setWinners(10);
+        PShuffle shuffle = builder.build();
+        byte[] body = shuffle.toByteArray();
+        log.info(getPath(uri));
+        try {
+            HttpUtils.getInstance().post(getPath(uri), HttpUtils.PROTO_BUF, body, cookies);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        PShuffle.
+//        List<PLocation> list = new ArrayList<PLocation>();
+//        PLocation p0 = PLocation.newBuilder().setLocCd("CN:0001").setLocNm("深圳").build();
+//        PLocation p1 = PLocation.newBuilder().setLocCd("CN:0002").setLocNm("广州").build();
+//        list.add(p0);
+//        list.add(p1);
+//        PLottery lottery = PLottery.newBuilder().setBonus(20).setTitle("好运气").setTotAmt(100).setType(0).setWinners(5).addAllUuidList(winners).addAllLocs(list).build();
+//        byte[] body = lottery.toByteArray();
+//        BaseHttpClient httpClient = new LocalHttpClient("127.0.0.1", 9014, path, null, body, "application/x-protobuf");
+//        PMessage message = httpClient.postBodyMethod();
+//        assertEquals("POrderPay", message.getType());
+
+        /*StringBuffer path = new StringBuffer("/v1/payment/lottery/create");
 	    List<String> winners = new ArrayList<String>();
 	    winners.add("1468419313301436961");
 	    winners.add("1468419313301436962");
@@ -55,12 +86,6 @@ public class LotteryProcessTest {
         BaseHttpClient httpClient = new LocalHttpClient("127.0.0.1", 9014, path, null, null, "application/x-protobuf");
         PMessage message = httpClient.getMethod();
         assertEquals("PLotteryListRet", message.getType());*/
-	    /**
-	    StringBuffer path = new StringBuffer("/customers/2");
-        BaseHttpClient httpClient = new LocalHttpClient("127.0.0.1", 8081, path, null, null, "application/x-protobuf");
-        PMessage message = httpClient.getMethod();
-        assertEquals("Josh", message.getType());
-        */
 	    /** 
 	    StringBuffer path = new StringBuffer("/v1/payment/lottery/detail?lotteryId=520170228286148971922067456&type=1&keywords=ff");
 	    BaseHttpClient httpClient = new LocalHttpClient("127.0.0.1", 9014, path, null, null, "application/x-protobuf");
@@ -70,12 +95,12 @@ public class LotteryProcessTest {
         System.out.println(ret.getLocs(0).getLocNm()+ "------------");
         assertEquals("PLotteryDetailRet", message.getType());*/
 	    
-	    StringBuffer path = new StringBuffer("/v1/payment/lottery/accept");
+	   /** StringBuffer path = new StringBuffer("/v1/payment/lottery/accept");
 	    PAcceptPrize prize = PAcceptPrize.newBuilder().setLotteryId("520170228286148971922067456").build();
         byte[] body = prize.toByteArray();
         BaseHttpClient httpClient = new LocalHttpClient("192.168.7.245", 9014, path, null, body, "application/x-protobuf");
         PMessage message = httpClient.postBodyMethod();
-        assertEquals("PResult", message.getType());
+        assertEquals("PResult", message.getType()); */
         /**http://127.0.0.1:8081/customers/1
         StringBuffer path = new StringBuffer("/v1/payment/lottery/list?startIndex=0&size=5&keywords=好");
         BaseHttpClient httpClient = new LocalHttpClient("127.0.0.1", 9014, path, null, null, "application/x-protobuf");
