26dee4e1eb78505109eb20004113946b990c8016
zhaoyongandy <andy.zhao@blemobi.com>
2017-03-08 20:51:47 +0800

    添加表格存储操作类

--- src/main/java/com/blemobi/payment/dao/TableStoreDao.java
+++ src/main/java/com/blemobi/payment/dao/TableStoreDao.java
@@ -1,5 +1,7 @@
 package com.blemobi.payment.dao;
 
+import com.alicloud.openservices.tablestore.model.Row;
+
 /**
  * 阿里云表格存储操作接口类
  * 
@@ -12,10 +14,23 @@ public interface TableStoreDao {
 	 * 查找成员是否存在
 	 * 
 	 * @param key
+	 *            表名称
+	 * @param key
 	 *            存储的KEY
 	 * @param member
 	 *            成员uuid
 	 * @return
 	 */
-	public boolean existsByKey(String key, String member);
+	public boolean existsByKey(String tableName, String key, String member);
+
+	/**
+	 * 查找行
+	 * 
+	 * @param key
+	 *            表名称
+	 * @param key
+	 *            存储的KEY
+	 * @return
+	 */
+	public Row selectByKey(String tableName, String key);
 }
--- src/main/java/com/blemobi/payment/dao/impl/TableStoreDaoImpl.java
+++ src/main/java/com/blemobi/payment/dao/impl/TableStoreDaoImpl.java
@@ -1,8 +1,15 @@
 package com.blemobi.payment.dao.impl;
 
-//github.com/blemobi/java-payment.git
 import org.springframework.stereotype.Repository;
 
+import com.alicloud.openservices.tablestore.SyncClient;
+import com.alicloud.openservices.tablestore.model.GetRowRequest;
+import com.alicloud.openservices.tablestore.model.GetRowResponse;
+import com.alicloud.openservices.tablestore.model.PrimaryKey;
+import com.alicloud.openservices.tablestore.model.PrimaryKeyBuilder;
+import com.alicloud.openservices.tablestore.model.PrimaryKeyValue;
+import com.alicloud.openservices.tablestore.model.Row;
+import com.alicloud.openservices.tablestore.model.SingleRowQueryCriteria;
 import com.blemobi.payment.dao.TableStoreDao;
 
 /**
@@ -13,26 +20,49 @@ import com.blemobi.payment.dao.TableStoreDao;
  */
 @Repository("tableStoreDao")
 public class TableStoreDaoImpl implements TableStoreDao {
-	// ACCESSID = "LTAIqjq4OplpzZRS"
-	// ACCESSKEY = "GbtpMcQHxTBElHLVwC4UDl1lSsmFK4"
+	final static String endPoint = "http://rongliang-test.cn-shenzhen.ots.aliyuncs.com";
+	final static String accessKeyId = "LTAIqjq4OplpzZRS";
+	final static String accessKeySecret = "GbtpMcQHxTBElHLVwC4UDl1lSsmFK4";
+	final static String instanceName = "rongliang-test";
 
-	// "http://rongliang-test.cn-shenzhen.ots-internal.aliyuncs.com"
-	// "http://rongliang-test.cn-shenzhen.ots-internal.aliyuncs.com"
-
-	// "http://xingneng-test.cn-shenzhen.ots-internal.aliyuncs.com"
-	// "http://xingneng-test.cn-shenzhen.ots-internal.aliyuncs.com"
-
-	final String endPoint = "http://rongliang-test.cn-shenzhen.ots.aliyuncs.com";
-	final String accessKeyId = "LTAIqjq4OplpzZRS";
-	final String accessKeySecret = "GbtpMcQHxTBElHLVwC4UDl1lSsmFK4";
-	final String instanceName = "rongliang-test";
-
-	//private SyncClient client = new SyncClient(endPoint, accessKeyId, accessKeySecret, instanceName);
+	private static SyncClient client = new SyncClient(endPoint, accessKeyId, accessKeySecret, instanceName);
 
 	@Override
-	public boolean existsByKey(String key, String member) {
-		// TODO Auto-generated method stub
+	public boolean existsByKey(String tableName, String key, String member) {
+		SingleRowQueryCriteria criteria = buildCriteria(tableName, key);
+		// 设置读取某些列
+		criteria.addColumnsToGet(member);
+		GetRowResponse getRowResponse = client.getRow(new GetRowRequest(criteria));
+		Row row = getRowResponse.getRow();
+		return !(row == null);
+	}
 
-		return true;
+	@Override
+	public Row selectByKey(String tableName, String key) {
+		SingleRowQueryCriteria criteria = buildCriteria(tableName, key);
+		GetRowResponse getRowResponse = client.getRow(new GetRowRequest(criteria));
+		Row row = getRowResponse.getRow();
+		return row;
+	}
+
+	/**
+	 * 读取条件
+	 * 
+	 * @param tableName
+	 *            表名称
+	 * @param key
+	 *            行的key
+	 * @return
+	 */
+	private SingleRowQueryCriteria buildCriteria(String tableName, String key) {
+		// 构造主键
+		PrimaryKeyBuilder primaryKeyBuilder = PrimaryKeyBuilder.createPrimaryKeyBuilder();
+		primaryKeyBuilder.addPrimaryKeyColumn("key", PrimaryKeyValue.fromString(key));
+		PrimaryKey primaryKey = primaryKeyBuilder.build();
+		// 读一行
+		SingleRowQueryCriteria criteria = new SingleRowQueryCriteria(tableName, primaryKey);
+		// 设置读取最新版本
+		criteria.setMaxVersions(1);
+		return criteria;
 	}
 }
