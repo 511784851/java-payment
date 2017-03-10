072354ea1b7729cec2bca638f4727f80a6e37297
HUNTER.POON <hunter.pan@foxmail.com>
2017-03-10 15:35:22 +0800

    添加融云回调验签

--- src/main/java/com/blemobi/payment/rest/RongYunNotifyProcess.java
+++ src/main/java/com/blemobi/payment/rest/RongYunNotifyProcess.java
@@ -50,89 +50,79 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @Path("v1/payment/callback")
 public class RongYunNotifyProcess {
-	private static final CallbackService callbackService = InstanceFactory.getInstance(CallbackService.class);
 
-	/**
-	 * @Description 支付成功通知
-	 * @author HUNTER.POON
-	 * @param respstat
-	 *            响应码
-	 * @param respmsg
-	 *            响应消息
-	 * @param orderAmount
-	 *            订单金额
-	 * @param orderNo
-	 *            订单号
-	 * @param orderStatus
-	 *            订单状态
-	 * @param orderTime
-	 *            订单时间
-	 * @param custOrderNo
-	 *            渠道订单号
-	 * @param receiveUid
-	 *            接收人唯一标示
-	 * @param sign
-	 *            摘要
-	 * @return
-	 */
-	@POST
-	@Path("notify")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaTypeExt.MULTIPART_FORM_DATA)
-	public String callback(String jsonString) {
-		log.debug("json str:" + jsonString);
-		JSONObject json = JSONObject.parseObject(jsonString);
-//		String respstat = json.getString("respstat");
-//		String respmsg = json.getString("respmsg");
-//		String orderAmount = json.getString("orderAmount");
-//		String orderNo = json.getString("orderNo");
-//		String orderStatus = json.getString("orderStatus");
-//		String orderTime = json.getString("orderTime");
-//		String custOrderNo = json.getString("custOrderNo");
-//		String receiveUid = json.getString("receiveUid");
-//		String sign = json.getString("sign");
+    private static final CallbackService callbackService = InstanceFactory.getInstance(CallbackService.class);
 
-		String sign = json.getString("sign");
-		String custOrderNo = json.getString("custOrderNo");
-		String orderNo = json.getString("orderNo");
-		String orderAmount = json.getString("orderAmount");
-		String orderTime = json.getString("orderTime");
-		String orderStatus = json.getString("orderStatus");
-		String returnType = json.getString("returnType");
-		String custUid = json.getString("custUid");
-		String receiveUid = json.getString("receiveUid");
-		
-		try {
-			Map<String, String> param = new HashMap<String, String>();
-			param.put("custOrderNo", custOrderNo);
-			param.put("orderNo", orderNo);
-			param.put("orderAmount", orderAmount);
-			param.put("orderTime", orderTime);
-			param.put("orderStatus", orderStatus);
-			param.put("returnType", returnType);
-			param.put("custUid", custUid);
-			param.put("receiveUid", receiveUid);
-			param.put("seckey", SignHelper.seckey);
+    /**
+     * @Description 支付成功通知
+     * @author HUNTER.POON
+     * @param respstat
+     *            响应码
+     * @param respmsg
+     *            响应消息
+     * @param orderAmount
+     *            订单金额
+     * @param orderNo
+     *            订单号
+     * @param orderStatus
+     *            订单状态
+     * @param orderTime
+     *            订单时间
+     * @param custOrderNo
+     *            渠道订单号
+     * @param receiveUid
+     *            接收人唯一标示
+     * @param sign
+     *            摘要
+     * @return
+     */
+    @POST
+    @Path("notify")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaTypeExt.MULTIPART_FORM_DATA)
+    public String callback(String jsonString) {
+        log.debug("json str:" + jsonString);
+        JSONObject json = JSONObject.parseObject(jsonString);
 
-			String signLocal = SignUtil.sign(param);
-			log.info(signLocal);
-			// 融云支付失败，不予处理
-//			if (!Constants.RESPSTS.SUCCESS.getValue().equals(respstat)
-//					|| !Constants.RONGYUN_ORD_STS.SUCCESS.getValue().equals(orderStatus)) {
-//				log.info("respstat:" + respstat);
-//				return Constants.HTMLSTS.SUCCESS.getValue();
-//			}
-//			// 验签失败
-//			if (signLocal == null || !signLocal.equals(sign)) {
-//				log.warn("signLocal：" + signLocal + ",sign:" + sign);
-//				return Constants.HTMLSTS.FAILED.getValue();
-//			}
-			callbackService.paySucc(orderAmount, DateTimeUtils.currTime(), custOrderNo, receiveUid, orderNo,
-					orderStatus, " ");
-		} catch (Exception ex) {
-			log.error("payment callback failed", ex);
-			return Constants.HTMLSTS.FAILED.getValue();
-		}
-		return Constants.HTMLSTS.SUCCESS.getValue();
-	}
+        String sign = json.getString("sign");
+        String custOrderNo = json.getString("custOrderNo");
+        String orderNo = json.getString("orderNo");
+        String orderAmount = json.getString("orderAmount");
+        String orderTime = json.getString("orderTime");
+        String orderStatus = json.getString("orderStatus");
+        String returnType = json.getString("returnType");
+        String custUid = json.getString("custUid");
+        String receiveUid = json.getString("receiveUid");
+
+        try {
+            Map<String, String> param = new HashMap<String, String>();
+            param.put("custOrderNo", custOrderNo);
+            param.put("orderNo", orderNo);
+            param.put("orderAmount", orderAmount);
+            param.put("orderTime", orderTime);
+            param.put("orderStatus", orderStatus);
+            param.put("returnType", returnType);
+            param.put("custUid", custUid);
+            param.put("receiveUid", receiveUid);
+            param.put("seckey", SignHelper.seckey);
+
+            String signLocal = SignUtil.sign(param);
+            log.info(signLocal);
+            // 融云支付失败，不予处理
+            if (!Constants.RONGYUN_ORD_STS.SUCCESS.getValue().equals(orderStatus)) {
+                return Constants.HTMLSTS.SUCCESS.getValue();
+            }
+            // 验签失败
+            if (signLocal == null || !signLocal.equals(sign)) {
+                log.warn("signLocal：" + signLocal + ",sign:" + sign);
+                return Constants.HTMLSTS.FAILED.getValue();
+            }
+            callbackService.paySucc(orderAmount, DateTimeUtils.currTime(), custOrderNo, receiveUid, orderNo,
+                    orderStatus, " ");
+        } catch (Exception ex) {
+            log.error("payment callback failed", ex);
+            return Constants.HTMLSTS.FAILED.getValue();
+        }
+        return Constants.HTMLSTS.SUCCESS.getValue();
+    }
 }
