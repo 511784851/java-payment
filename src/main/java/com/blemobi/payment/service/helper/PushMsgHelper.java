f6c33a8ed1f2ea20d9553e56d1e36cb57dae4d6f
zhaoyongandy <andy.zhao@blemobi.com>
2017-03-24 14:18:18 +0800

    增加实物抽奖消息功能

--- src/main/java/com/blemobi/payment/service/helper/PushMsgHelper.java
+++ src/main/java/com/blemobi/payment/service/helper/PushMsgHelper.java
@@ -1,8 +1,29 @@
 package com.blemobi.payment.service.helper;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.simple.JSONObject;
+
+import com.blemobi.library.client.ChatHttpClient;
+import com.blemobi.library.grpc.NotifyGrpcClient;
 import com.blemobi.library.grpc.RobotGrpcClient;
+import com.blemobi.library.util.CommonUtil;
+import com.blemobi.sep.probuf.NotificationApiProtos.PNotifyInternalMessage;
+import com.blemobi.sep.probuf.NotificationApiProtos.PNotifyInternalMessageList;
+import com.blemobi.sep.probuf.NotificationProtos.ENotifyType;
+import com.blemobi.sep.probuf.NotificationProtos.PNotifyMessage;
+import com.blemobi.sep.probuf.NotificationProtos.PNotifyRawMessage;
+import com.blemobi.sep.probuf.NotificationProtos.PNotifySimple;
+import com.blemobi.sep.probuf.PaymentProtos.PUserBaseGiftEx;
+import com.blemobi.sep.probuf.ResultProtos.PMessage;
+import com.blemobi.sep.probuf.ResultProtos.PResult;
 import com.blemobi.sep.probuf.RobotApiProtos.PBLotteryNotifyMsg;
 import com.blemobi.sep.probuf.RobotApiProtos.PBRedPacketNotifyMsg;
 import com.blemobi.sep.probuf.RobotApiProtos.PRobotNotifyMsg;
@@ -10,11 +31,14 @@ import com.blemobi.sep.probuf.RobotApiProtos.PRobotNotifyMsgList;
 import com.blemobi.sep.probuf.RobotApiProtos.PRobotRawNotifyMsg;
 import com.blemobi.sep.probuf.RobotProtos.ERobotPushType;
 
+import lombok.extern.log4j.Log4j;
+
 /**
  * 业务消息推送
  * 
  * @author zhaoyong
  */
+@Log4j
 public class PushMsgHelper {
 
 	private String send_uuid;
@@ -59,7 +83,7 @@ public class PushMsgHelper {
 	}
 
 	/**
-	 * 推送红包消息
+	 * 推送红包消息（智能机器人）
 	 */
 	public void redPacketMsg() {
 		// 红包信息
@@ -72,19 +96,36 @@ public class PushMsgHelper {
 	}
 
 	/**
-	 * 推送抽奖消息
+	 * 推送抽奖消息（智能机器人）
 	 */
 	public void lotteryMsg() {
+		lotteryMsg(ERobotPushType.Lottery);
+	}
+
+	/**
+	 * 推送抽奖消息（智能机器人）
+	 * 
+	 * ERobotPushType 枚举类型定义如下 
+	 * Regards 息推送 
+	 * Remind 生日提醒 
+	 * Lottery 抽奖 
+	 * RedPacket 红包
+	 * Stream 直播 
+	 * LotteryRemind 领奖提醒 
+	 * LotteryExpire 抽奖过期提醒 
+	 * LotteryCourier
+	 */
+	public void lotteryMsg(ERobotPushType type) {
 		// 红包信息
 		PBLotteryNotifyMsg lotteryNotifyMsg = PBLotteryNotifyMsg.newBuilder().setOrdNo(ord_no).setText(desc).build();
 		// 消息内容
 		PRobotRawNotifyMsg robotRawNotifyMsg = PRobotRawNotifyMsg.newBuilder().setLottery(lotteryNotifyMsg).build();
-		push(robotRawNotifyMsg, ERobotPushType.Lottery);
+		push(robotRawNotifyMsg, type);
 
 	}
 
 	/**
-	 * 推送
+	 * 推送（智能机器人）
 	 * 
 	 * @param robotRawNotifyMsg
 	 *            消息内容
@@ -106,5 +147,102 @@ public class PushMsgHelper {
 
 		RobotGrpcClient client = new RobotGrpcClient();
 		client.push(robotNotifyMsgList);
+	}
+
+	/**
+	 * 抽奖发货提醒消息（系统）
+	 */
+	public void sendShipping() {
+		StringBuffer url = new StringBuffer("payment://lottery/shipping?");
+		url.append("lottery_id=");
+		url.append(ord_no);
+		send(url.toString());
+	}
+
+	/**
+	 * 更新领奖资料提醒消息（系统）
+	 * 
+	 * @param userBaseGiftEx
+	 */
+	public void sendEditrcv(PUserBaseGiftEx userBaseGiftEx) {
+		StringBuffer url = new StringBuffer("payment://lottery/editrcv?");
+		url.append("uuid=");
+		url.append(userBaseGiftEx.getInfo().getUUID());
+		url.append("&lottery_id=");
+		url.append(userBaseGiftEx.getGift().getGiftId());
+		url.append("&gift_nm=");
+		url.append(userBaseGiftEx.getGift().getGiftNm());
+		url.append("&rcv_nm=");
+		url.append(userBaseGiftEx.getRcvNm());
+		url.append("&rcv_addr=");
+		url.append(userBaseGiftEx.getRcvAddr());
+		url.append("&rcv_phone=");
+		url.append(userBaseGiftEx.getRcvPhone());
+		url.append("&rcv_email=");
+		url.append(userBaseGiftEx.getRcvEmail());
+		url.append("&rcv_remark=");
+		url.append(userBaseGiftEx.getRcvRemark());
+		send(url.toString());
+	}
+
+	/**
+	 * 推送（系统）
+	 * 
+	 * @param url
+	 *            消息url已经参数
+	 */
+	@SuppressWarnings("deprecation")
+	public void send(String url) {
+		url = URLEncoder.encode(url);
+		PNotifySimple notifySimple = PNotifySimple.newBuilder().setUri(url).build();
+
+		PNotifyRawMessage notifyRawMessage = PNotifyRawMessage.newBuilder().setSimple(notifySimple).build();
+
+		PNotifyMessage notifyMessage = PNotifyMessage.newBuilder().setType(ENotifyType.SimpleMessage)
+				.setTime(System.currentTimeMillis()).setContent(notifyRawMessage).build();
+
+		PNotifyInternalMessage notifyInternalMessage = PNotifyInternalMessage.newBuilder().setStateless(true)
+				.setService("payment").setMessage(notifyMessage).addAllRecipient(toList).build();
+
+		PNotifyInternalMessageList request = PNotifyInternalMessageList.newBuilder().addList(notifyInternalMessage)
+				.build();
+
+		NotifyGrpcClient client = new NotifyGrpcClient();
+		client.send(request);
+	}
+
+	/**
+	 * 实物抽奖即将过期未领取推送消息（阿里）
+	 * 
+	 * @param conent
+	 *            消息提示内容
+	 * @throws IOException
+	 */
+	public void pushOver(String conent) throws IOException {
+		Map<String, String> info = new HashMap<String, String>();
+		info.put("MsgType", "ov");
+		info.put("Title", "BB");
+		info.put("Id", ord_no);
+
+		String infoString = JSONObject.toJSONString(info);
+
+		Map<String, String> param = new HashMap<String, String>();
+		param.put("info", infoString);
+
+		String receiverUUIDs = CommonUtil.collectionToString(toList);
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("receiverUUIDs", receiverUUIDs));
+		params.add(new BasicNameValuePair("title", "BB"));
+		params.add(new BasicNameValuePair("msgid", "ov"));
+		params.add(new BasicNameValuePair("description", ""));
+		params.add(new BasicNameValuePair("alertContent", conent));
+		params.add(new BasicNameValuePair("info", infoString));
+
+		ChatHttpClient httpClient = new ChatHttpClient();
+		PMessage message = httpClient.multi(params);
+		PResult result = PResult.parseFrom(message.getData());
+		if (result.getErrorCode() != 0) {
+			log.error("推送消息失败  -> " + result.getErrorCode());
+		}
 	}
 }
