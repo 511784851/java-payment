86edd587c3dd9f4952b26c31edfb24291dcc60d1
zhaoyongandy <andy.zhao@blemobi.com>
2017-04-06 14:47:24 +0800

    雕饰Consul

--- src/main/java/com/blemobi/payment/core/PaymentManager.java
+++ src/main/java/com/blemobi/payment/core/PaymentManager.java
@@ -2,14 +2,12 @@ package com.blemobi.payment.core;
 
 import java.util.List;
 
-import com.blemobi.library.cache.CacheInvalid;
-import com.blemobi.library.cache.LiveThread;
-import com.blemobi.library.consul.BaseService;
-import com.blemobi.library.consul.ConsulManager;
-import com.blemobi.library.health.HealthManager;
+import com.blemobi.library.consul_v1.Constants.CONFIG_KV_KEY;
+import com.blemobi.library.consul_v1.ConsulClientMgr;
+import com.blemobi.library.consul_v1.ConsulServiceMgr;
+import com.blemobi.library.consul_v1.PropsUtils;
 import com.blemobi.library.jetty.JettyServer;
 import com.blemobi.library.jetty.ServerFilter;
-import com.blemobi.library.log.LoggerManager;
 import com.blemobi.payment.bat.RefundThread;
 
 import lombok.extern.log4j.Log4j;
@@ -18,10 +16,11 @@ import lombok.extern.log4j.Log4j;
  * 服务启动管理类
  * 
  * @author zhaoyong
- *
  */
 @Log4j
 public class PaymentManager {
+	// private static String ADDRESS = "192.168.7.245";//本地环境
+	private static String ADDRESS = "127.0.0.1";// 测试、生产环境
 	/**
 	 * 服务名称
 	 */
@@ -30,40 +29,22 @@ public class PaymentManager {
 	 * 要发布rest服务的
 	 */
 	private static final String packages = "com.blemobi." + selfName + ".rest";
-	/**
-	 * 连接Consul服务器的间隔时间
-	 */
-	private static long consulIntervalTime = 1000 * 30;
-	/**
-	 * 用户缓存失效时间（单位：毫秒）
-	 */
-	private static final long live_time = 1 * 24 * 60 * 60 * 1000;
 
 	public static void main(String[] args) throws Exception {
-		// 初始化Consul
-		ConsulManager.startService(selfName, args, consulIntervalTime); // 启动连接Consul服务
-		// 启动Jetty HTTP服务器
-		startJetty();
-		// 发布Consul的健康发现
-		startHealth();
-		// 启动线程管理用户缓存
-		LiveThread LiveThread = new LiveThread(live_time, new CacheInvalid());
-		LiveThread.start();
+		ConsulClientMgr.initial(args, selfName, ADDRESS);
+		log.info("consul client initialed");
+		Integer jettyPort = PropsUtils.getInteger(CONFIG_KV_KEY.JETTY_PORT);
+		log.info("jetty port:" + jettyPort);
+		if (!ConsulClientMgr.getENV_TYPE().equalsIgnoreCase("local")) {
+			Integer healthPort = PropsUtils.getInteger(CONFIG_KV_KEY.HEALTH_CHECK_PORT);
+			log.info("health check port:" + healthPort);
+			ConsulServiceMgr.registerServiceWithHealthChk(jettyPort, selfName, healthPort, selfName, null);
+		}
+		startJetty(jettyPort);
 		log.info("Start Payment Server Finish!");
-		// 初始化Consul日志管理
-		LoggerManager.startService();
-		//退款扫描
+		// 退款扫描
 		RefundThread rt = new RefundThread();
-        (new Thread(rt)).start();
-	}
-
-	/**
-	 * 发布Consul的健康发现
-	 */
-	private static void startHealth() {
-		String health_check_port = BaseService.getProperty("health_check_port");
-		int check_port = Integer.parseInt(health_check_port);
-		HealthManager.startService(check_port, selfName);
+		(new Thread(rt)).start();
 	}
 
 	/**
@@ -71,10 +52,7 @@ public class PaymentManager {
 	 * 
 	 * @throws Exception
 	 */
-	private static void startJetty() throws Exception {
-		// 服务启动端口
-		String jetty_port = BaseService.getProperty("jetty_port");
-		int port = Integer.parseInt(jetty_port);
+	private static void startJetty(Integer port) throws Exception {
 		// 过滤器配置
 		FilterProperty filterProperty = new FilterProperty();
 		List<ServerFilter> serverFilterList = filterProperty.getFilterList();
