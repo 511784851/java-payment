a385b8e80ee0e1e1591c187a04e57d1ba270d96f
zhaoyongandy <andy.zhao@blemobi.com>
2017-05-26 16:58:36 +0800

    升级公共库版本号1.5.54

--- src/main/java/com/blemobi/payment/core/PaymentManager.java
+++ src/main/java/com/blemobi/payment/core/PaymentManager.java
@@ -2,13 +2,11 @@ package com.blemobi.payment.core;
 
 import java.util.List;
 
-import com.blemobi.library.cache.CacheInvalid;
-import com.blemobi.library.cache.LiveThread;
-import com.blemobi.library.consul_v1.Constants.CONFIG_KV_KEY;
+import com.blemobi.library.cache.CacheListener;
 import com.blemobi.library.consul_v1.Constants;
+import com.blemobi.library.consul_v1.Constants.CONFIG_KV_KEY;
 import com.blemobi.library.consul_v1.ConsulClientMgr;
 import com.blemobi.library.consul_v1.ConsulServiceMgr;
-import com.blemobi.library.consul_v1.HeartBeatThread;
 import com.blemobi.library.consul_v1.PropsUtils;
 import com.blemobi.library.jetty.JettyServer;
 import com.blemobi.library.jetty.ServerFilter;
@@ -23,7 +21,7 @@ import lombok.extern.log4j.Log4j;
  */
 @Log4j
 public class PaymentManager {
-	//private static String ADDRESS = "192.168.7.245";//本地环境
+	// private static String ADDRESS = "192.168.7.245";//本地环境
 	private static String ADDRESS = "127.0.0.1";// 测试、生产环境
 	/**
 	 * 服务名称
@@ -34,9 +32,13 @@ public class PaymentManager {
 	 */
 	private static final String packages = "com.blemobi." + selfName + ".rest";
 	/**
-	 * 用户缓存失效时间（单位：毫秒）
+	 * 失效时间（单位：毫秒）
 	 */
-	private static final long live_time = 1 * 24 * 60 * 60 * 1000;
+	private static final long invalid_time = 3 * 24 * 60 * 60 * 1000;
+	/**
+	 * 扫描间隔时间（单位：毫秒）
+	 */
+	private static final long sleep_time = 1 * 24 * 60 * 60 * 1000;
 
 	public static void main(String[] args) throws Exception {
 		Constants.SERVER_NM = selfName;
@@ -53,8 +55,10 @@ public class PaymentManager {
 		log.info("Start Payment Server Finish!");
 
 		// 启动线程管理用户缓存
-		LiveThread LiveThread = new LiveThread(live_time, new CacheInvalid());
-		LiveThread.start();
+		new CacheListener(invalid_time, sleep_time, (uuid) -> {
+			log.debug("用户缓存信息过期了！ uuid=" + uuid);
+		}).run();
+
 		// 退款扫描
 		RefundThread rt = new RefundThread();
 		(new Thread(rt)).start();
